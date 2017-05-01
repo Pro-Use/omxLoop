70943ed17c2384564c4953792cbfca61c97f1bf5
pro-use <robertprouse@gmail.com>
2017-05-01 13:00:27 +0100

    check if audio file

--- omxLoop.py
+++ omxLoop.py
@@ -14,35 +14,37 @@ class OMXcontrol(object):
                 if not self.INSTANCES[instance].isAlive():
                     del self.INSTANCES[instance]
 
-    def __start(self, playerNum, future):
-            self.INSTANCES[playerNum].initialize()
-            wait = float(future) - time.time()
+    def __start(self, playerNum, future, af):
+            print('initialised %s' % self.INSTANCES[playerNum].initialize())
+            if af is True:
+                print('seek to start %s' % self.INSTANCES[playerNum].setPosition(0))
+                wait = (float(future) - time.time()) - 0.5
+            else:
+                wait = float(future) - time.time()
             print('sleeping %s' % wait)
             if wait > 0:
                 sleep(wait)
             else:
                 sleep(0.1)
-            print('alpha = %s' % self.INSTANCES[playerNum].setAlpha(255))
+            if af is False:
+                print('alpha = %s' % self.INSTANCES[playerNum].setAlpha(255))
             print('play = %s' % self.INSTANCES[playerNum].playPause())
             print('volume = %s' % self.INSTANCES[playerNum].setVolume(0))
 
-    def loadfile(self, mediafile, future=0, loop=True, args=None, replace=True):
+    def loadfile(self, mediafile, future=0, loop=True, args=None, replace=True, af=False):
         ARGS = ['--vol', '-6000']
         if args is not None:
             ARGS += args
         if loop is True:
             ARGS += ['--loop']
         self.__cleanup()
-        if len(self.INSTANCES) == 0:
-            dbusNUM = 0
-        else:
-            c = 0
-            while c in self.INSTANCES.keys():
-                c += 1
-            dbusNUM = c
+        dbusNUM = 0
+        if len(self.INSTANCES) != 0:
+            while dbusNUM in self.INSTANCES.keys():
+                dbusNUM += 1
         player = Player(dbusNUM, mediafile, args=ARGS)
         self.INSTANCES[dbusNUM] = player
-        self.__start(dbusNUM, future)
+        self.__start(dbusNUM, future, af)
         if replace is True:
             sleep(0.2)
             for instance in self.INSTANCES.keys():
